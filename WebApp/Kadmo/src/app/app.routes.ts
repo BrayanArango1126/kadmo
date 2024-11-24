30494f19f99fd19d54a302d6c00030b802ca2e85
santigalloovalle <EGALLO@UNIEMPRESARIAL.EDU.CO>
2024-11-24 01:34:31 -0500

    inicio front
    Se cambio el proyecto de standalone a un proyecto de modulos

--- WebApp/Kadmo/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { RegisterComponent } from './components/register/register.component';
-
-export const routes: Routes = [
-  {path: 'register', component: RegisterComponent}
-];
