4bf9bae217fbf4803d89d16a36c408f59a245e0b
santigalloovalle <EGALLO@UNIEMPRESARIAL.EDU.CO>
2024-11-27 00:07:30 -0500

    Vista general libros

--- kadmo/src/app/app-routing.module.ts
+++ kadmo/src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { StoreComponent } from './components/store/store.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { NotFoundComponent } from './components/error/not-found/not-found.component';
+import { BooksComponent } from './components/store/books/books.component';
+import { StoreComponent } from './components/store/store.component';
 
 const routes: Routes = [
 // {path:'admin', loadChildren: () => import('./components/admin/admin.module').then(m => m.AdminModule)},
 {path:'', component: StoreComponent},
 {path:'login', component: LoginComponent}, 
 {path:'register', component: RegisterComponent}, 
+{path:'store', loadChildren: () => import('./components/store/store.module').then(m => m.StoreModule)},
 {path:'404', component: NotFoundComponent}, 
 {path:'**', redirectTo: '/404'}, 
 ];
--- kadmo/src/app/components/store/store.module.ts
+++ kadmo/src/app/components/store/store.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreComponent } from './store.component';
-import { AppRoutingModule } from '../../app-routing.module';
-import { HomeComponent } from './home/home.component';
 import { MainSectionComponent } from './home/pages/main-section/main-section.component';
 import { BestSellerSectionComponent } from './home/pages/best-seller-section/best-seller-section.component';
 import { CategoriesSectionComponent } from './home/pages/categories-section/categories-section.component';
 import { ResenasSectionComponent } from './home/pages/resenas-section/resenas-section.component';
+import { BooksComponent } from './books/books.component';
+import { HomeComponent } from './home/home.component';
+import { StoreRoutingModule } from './store-routing.module';
 
 
 @NgModule({
@@ -17,10 +18,11 @@ import { ResenasSectionComponent } from './home/pages/resenas-section/resenas-se
     BestSellerSectionComponent,
     CategoriesSectionComponent,
     ResenasSectionComponent,
+    BooksComponent,
   ],
   imports: [
     CommonModule,
-    AppRoutingModule
+    StoreRoutingModule,
   ]
 })
 export class StoreModule { }
